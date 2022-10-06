65419ff9272fd2265fe7a456e5ece362a94d6e37
esma3 <semababayeva9898@gmail.com>
2022-10-07 01:44:40 +0400

    first commit

---
+++ js/slider.js
@@ -0,0 +1,114 @@
+// var ad ="rza"
+// var age =2
+// var qaliq =2.50
+// var status=true
+// console.log(ad +qaliq)
+
+
+
+
+// const car =[]
+// car[1] = "BMW"
+// car[2]="Mercesdes"
+// car[3]="Ford"
+// console.log(car[2])
+"use strict"
+
+
+
+//menim adim _ soyadim ise _ dir. Men _tarixinde _ dunyaya gelmisem . MEnim sevdiyim film _dir . hal hazirda cari il - doguldudugu il 
+//yasim var var . Men __  olacam 
+
+var ad="Sema"
+var result2=ad.fontsize(5)
+
+var soyad="Babayeva"
+// var result3=soyad.fontWeight("900")
+var age=24
+var year= 2022
+var birth=1998
+var film= "Lord of the Rings"
+let vezife="Web Developer"
+
+
+// console.log(ad+soyad+age)
+// document.getElementsByTagName("div")[5].innerHTML="sema"
+document.getElementsByTagName("p")[0].innerHTML="Menim adim "+"<span style='color:red'>"+ad+"</span>"+"dır."+" Menim soyadım isə "+"<span style='font-Weight:900'>"+soyad+"</span>"+"dır."+" Hal-hazirda "+"<span style='color:red'>"+(year-birth)+"</span>"+" yaşım var."+"En sevdiyim film "
++film+"dir."+" Men "+vezife+" olacam."
+// document.getElementsByTagName("p")[0].style.fontWeight="900";
+
+
+/////RENG DEYISSSSSDIRMEKKKK
+let text="Helloworld"
+// let result = text.fontcolor("green")
+let result1 = text.fontsize(7)
+
+document.getElementById("demo1").innerHTML="<span style='color:green'>"+result1+"</span>";
+document.getElementById("demo1").style.fontWeight="900";
+// document.getElementById("demo2").innerHTML =
+// "The temperature is " + toCelsius(77) + " Celsius";
+
+// function toCelsius(fahrenheit) {
+//   return (5/9) * (fahrenheit-32);
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function adi_goster(){
+//     document.getElementById("demo2").innerHTML="Esmira muellim"
+// }
+// document.getElementById("demo3").innerHTML="Sebuhi mellim"
+
+
+// document.getElementsByClassName("demo")[2].innerHTML = "Azime"
+// document.getElementsByClassName("demo")[0].innerHTML="nese"
+// document.getElementsByTagName("div")[2].innerHTML="adsuz"
+
+
+
+
+// document.getElementsByTagName("div")[1].innerHTML="<p style='color:red'>HElloWrld</p>"
+// document.getElementsByTagName("")[0].innerHTML="<p style='color:red'>HElloWrld</p>"
